d08ff32e029d208533da09569d25ae5cd5b58206
Christopher Baker <cgbaker@gmail.com>
2017-02-07 16:35:29 -0500

    updated ci build

---
+++ config-and-serve.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+<nginx_proxy.conf.template \
+    | sed "s|META_API_URL|$META_API_URL|" \
+    | sed "s|SEC_API_URL|$SEC_API_URL|" \
+    > default.conf 
+echo "default.conf"
+cat default.conf
+# nginx -g 'daemon off;'
+exec nginx '-g' 'daemon off;'
