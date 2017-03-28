53e3b8865ef25af442ee4cf54f0e95def14efc8b
jbetancur <johnnyazee@gmail.com>
2017-03-28 18:46:13 -0400

    fix provider create issue

---
+++ src/modules/MetaResource/sagas/index.test.js
@@ -0,0 +1,12 @@
+import sagas from './index';
+
+describe('Sagas Index', () => {
+  let result;
+  const rootSaga = sagas();
+
+  // This test is to mostly appease test coverage
+  it('should load metaSagas', () => {
+    result = rootSaga.next();
+    expect(result.value.length).to.equal(5);
+  });
+});
