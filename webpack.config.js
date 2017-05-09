0efd70411222b215d0db69895056678b0985dab4
jbetancur <johnnyazee@gmail.com>
2017-05-09 10:59:17 -0400

    support gzip bundles

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Clean = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 const merge = require('webpack-merge');
 const parts = require('./webpack.parts');
 
@@ -40,13 +41,19 @@ const common = merge([
         title: pkg.title,
         favicon: `${PATHS.srcPath}/assets/icons/favicon.ico`,
         template: `${PATHS.srcPath}/index.html`,
-        hash: true,
         inject: 'body',
       }),
       new CircularDependencyPlugin({
         exclude: /a\.js|node_modules/, // exclude node_modules
         failOnError: false, // show a warning when there is a circular dependency
       }),
+      new CompressionPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.(js|css|html|ico|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      })
     ],
   },
   parts.babelConfig(PATHS),
