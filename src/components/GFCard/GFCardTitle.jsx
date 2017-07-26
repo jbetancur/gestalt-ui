ddb079a466475c61992f3ed7c18ea6e8935122ea
John Betancur <johnnyazee@gmail.com>
2017-07-26 13:49:36 -0400

    fix card width on smaller screens

--- src/components/GFCard/GFCardTitle.jsx
+++ src/components/GFCard/GFCardTitle.jsx
@@ -1,16 +1,21 @@
 import styled from 'styled-components';
 import CardTitle from 'react-md/lib/Cards/CardTitle';
 
+// TODO: Write our own Card component
 const EnhancedCardTitle = styled(CardTitle)`
   &.md-card-title {
       padding-top: 0px;
       padding-left: 23px;
       padding-right: 23px;
   }
-  
+
   .md-card-title--title {
     max-width: 24em;
     word-break: break-word;
+
+    &.md-card-title--large {
+      font-size: 21px;
+    }
   }
 `;
 
