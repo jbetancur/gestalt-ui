60224462ae9854d5cb8f118110d5cc8dd35cad58
John Betancur <johnnyazee@gmail.com>
2017-09-11 17:49:51 -0400

    convert key svg icons to svg components for increased ui response

--- src/components/Icons/GestaltIcon/index.jsx
+++ src/components/Icons/GestaltIcon/index.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
-import Isvg from 'react-inlinesvg';
-import iconSVG from 'assets/icons/gf-logo1.svg';
+import styled from 'styled-components';
+import SVGIcon from 'react-md/lib/SVGIcons';
 
-const GalacticFogIcon = () => (
-  <Isvg src={iconSVG} />
+const SVGLogoStyled = styled(SVGIcon)`
+  .logo-trans-1{fill:#77e0f2;}.logo-trans-2{fill:#4dd7ee;}.logo-trans-3{fill:#5edbf0;}.logo-trans-4{fill:#75e0f2;}.logo-trans-5{fill:#5cdaf0;}.logo-trans-6{fill:#9ae8f5;}.logo-trans-7{fill:#67ddf1;}.logo-trans-8{fill:#92e6f5;}.logo-trans-9{fill:#b7eff8;}.logo-trans-10{fill:#adedf7;}.logo-trans-11{fill:#56d9ef;}.logo-trans-12{fill:#8be5f4;}.logo-trans-13{fill:#d2f5fb;}.logo-trans-14{fill:#9de9f6;}.logo-trans-15{fill:#c5f2fa;}.logo-trans-16{fill:#b8eff8;}.logo-trans-17{fill:#61dbf0;}.logo-trans-18{fill:#4fd7ef;}.logo-trans-19{fill:#5adaf0;}.logo-trans-20{fill:#6cdef1;}.logo-trans-21{fill:#4ad6ee;}.logo-trans-22{fill:#69ddf1;}.logo-trans-23{fill:#86e4f4;}.logo-trans-24{fill:#58d9ef;}.logo-trans-25{fill:#6edef1;}.logo-trans-26{fill:#d5f6fb;}.logo-trans-27{fill:#c3f1f9;}.logo-trans-28{fill:#a1eaf6;}.logo-trans-29{fill:#90e6f5;}.logo-trans-30{fill:none;}.logo-trans-31{fill:#72dff2;}
+`;
+
+/* eslint react/jsx-indent: 0 */
+const HierarchyIcon = props => (
+  <SVGLogoStyled viewBox="0 0 100.39 115.92" {...props}>
+    <g id="Layer_2" data-name="Layer 2">
+      <g id="Layer_1-2" data-name="Layer 1">
+        <polygon className="logo-trans-1" points="50.19 19.32 50.19 0 66.92 9.66 50.19 19.32" />
+        <polygon className="logo-trans-2" points="66.92 9.66 66.92 28.98 50.19 19.32 66.92 9.66" />
+        <polygon className="logo-trans-3" points="66.92 28.98 66.92 9.66 83.65 19.32 66.92 28.98" />
+        <polygon className="logo-trans-4" points="83.65 19.32 83.65 38.64 66.92 28.98 83.65 19.32" />
+        <polygon className="logo-trans-3" points="66.92 28.98 66.92 48.3 50.19 38.64 66.92 28.98" />
+        <polygon className="logo-trans-5" points="66.92 48.3 66.92 28.98 83.65 38.64 66.92 48.3" />
+        <polygon className="logo-trans-6" points="50.19 57.96 50.19 38.64 66.92 48.3 50.19 57.96" />
+        <polygon className="logo-trans-7" points="83.65 38.64 83.65 19.32 100.39 28.98 83.65 38.64" />
+        <polygon className="logo-trans-8" points="100.39 67.62 100.39 86.94 83.65 77.28 100.39 67.62" />
+        <polygon className="logo-trans-9" points="83.65 77.28 83.65 57.96 100.39 67.62 83.65 77.28" />
+        <polygon className="logo-trans-10" points="100.38 48.3 100.39 67.62 83.65 57.96 100.38 48.3" />
+        <polygon className="logo-trans-11" points="83.65 57.96 83.65 38.64 100.38 48.3 83.65 57.96" />
+        <polygon className="logo-trans-12" points="83.65 38.64 83.65 57.96 66.92 48.3 83.65 38.64" />
+        <polygon className="logo-trans-1" points="100.38 28.98 100.38 48.3 83.65 38.64 100.38 28.98" />
+        <polygon className="logo-trans-13" points="50.19 77.28 50.19 57.96 66.92 67.62 50.19 77.28" />
+        <polygon className="logo-trans-14" points="83.65 77.28 83.65 96.6 66.92 86.94 83.65 77.28" />
+        <polygon className="logo-trans-15" points="66.92 86.94 66.92 106.27 50.19 96.6 66.92 86.94" />
+        <polygon className="logo-trans-9" points="66.92 106.27 66.92 86.94 83.65 96.6 66.92 106.27" />
+        <polygon className="logo-trans-16" points="50.19 115.92 50.19 96.6 66.92 106.27 50.19 115.92" />
+        <polygon className="logo-trans-17" points="83.65 96.6 83.65 77.28 100.39 86.94 83.65 96.6" />
+        <polygon className="logo-trans-1" points="50.2 38.64 50.2 57.96 33.46 48.3 50.2 38.64" />
+        <polygon className="logo-trans-18" points="16.73 38.64 16.73 19.32 33.46 28.98 16.73 38.64" />
+        <polygon className="logo-trans-19" points="33.46 28.98 33.47 9.66 50.2 19.32 33.46 28.98" />
+        <polygon className="logo-trans-20" points="33.47 9.66 33.46 28.98 16.73 19.32 33.47 9.66" />
+        <polygon className="logo-trans-21" points="50.2 0 50.2 19.32 33.47 9.66 50.2 0" />
+        <polygon className="logo-trans-11" points="16.73 19.32 16.73 38.64 0 28.98 16.73 19.32" />
+        <polygon className="logo-trans-22" points="0 48.3 0 28.98 16.73 38.64 0 48.3" />
+        <polygon className="logo-trans-23" points="16.73 38.64 16.73 57.96 0 48.3 16.73 38.64" />
+        <polygon className="logo-trans-24" points="0 67.62 0 48.3 16.73 57.96 0 67.62" />
+        <polygon className="logo-trans-17" points="16.73 57.96 16.73 77.28 0 67.62 16.73 57.96" />
+        <polygon className="logo-trans-25" points="0 86.94 0 67.62 16.73 77.28 0 86.94" />
+        <polygon className="logo-trans-23" points="33.47 67.62 33.46 48.3 50.2 57.96 33.47 67.62" />
+        <polygon className="logo-trans-26" points="50.2 96.6 50.2 115.92 33.46 106.26 50.2 96.6" />
+        <polygon className="logo-trans-27" points="33.46 106.26 33.46 86.94 50.2 96.6 33.46 106.26" />
+        <polygon className="logo-trans-28" points="33.46 86.94 33.46 106.26 16.73 96.6 33.46 86.94" />
+        <polygon className="logo-trans-29" points="16.73 96.6 16.73 77.28 33.46 86.94 16.73 96.6" />
+        <polygon className="logo-trans-30" points="50.19 57.96 66.92 67.62 50.2 77.28 33.47 67.62 33.46 48.3 50.19 38.64 50.2 38.64 66.92 28.98 50.2 19.33 50.2 19.32 33.46 28.98 16.73 38.64 16.73 57.96 16.73 77.28 33.46 86.94 50.19 96.59 50.19 96.6 66.92 86.94 83.65 77.28 83.65 57.96 66.92 48.3 50.19 57.96" />
+        <polygon className="logo-trans-26" points="50.2 57.96 50.2 77.28 33.47 67.62 50.2 57.96" />
+        <polygon className="logo-trans-31" points="16.73 77.28 16.73 96.6 0 86.94 16.73 77.28" />
+        <line className="logo-trans-30" x1="50.19" y1="57.96" x2="66.92" y2="48.3" />
+      </g>
+    </g>
+  </SVGLogoStyled>
 );
 
-export default GalacticFogIcon;
+export default HierarchyIcon;
