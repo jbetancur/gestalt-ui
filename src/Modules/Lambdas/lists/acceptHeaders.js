100debda57e5e1297476f6119fac43b577524dc5
John Betancur <johnnyazee@gmail.com>
2017-11-02 15:00:06 -0400

    fix accept headers

--- src/Modules/Lambdas/lists/acceptHeaders.js
+++ src/Modules/Lambdas/lists/acceptHeaders.js
@@ -5,13 +5,23 @@ export default [
     format: 'text/plain'
   },
   {
+    displayName: 'text/html',
+    value: 'text/html',
+    format: 'text/html'
+  },
+  {
     displayName: 'application/js',
     value: 'application/js',
     format: 'application/js'
   },
   {
-    displayName: 'text/html',
-    value: 'text/html',
-    format: 'text/html'
-  }
+    displayName: 'application/json',
+    value: 'application/json',
+    format: 'application/json'
+  },
+  {
+    displayName: 'application/xml',
+    value: 'application/xml',
+    format: 'application/xmljs'
+  },
 ];
