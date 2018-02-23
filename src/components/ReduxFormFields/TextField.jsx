4ea1929905c7d46defcb961980805abb75c292f6
John Betancur <johnnyazee@gmail.com>
2018-02-23 18:44:27 -0500

    workaround for cursor jumping in TextField when using redux-form/react-md

--- src/components/ReduxFormFields/TextField.jsx
+++ src/components/ReduxFormFields/TextField.jsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { TextField } from 'react-md';
 
-/* eslint-disable react/prop-types */
-const InputField = ({ input, meta: { touched, error }, ...others }) => (
-  <TextField
-    id={input.name}
-    error={touched && !!error}
-    errorText={error}
-    {...input}
-    {...others}
-  />
-);
+// TODO: fixes issue where cursor jumps when maxLength is used on an input text together with redux-form
 
-InputField.defaultProps = {
-  lineDirection: 'center',
-  fullWidth: true,
-};
+/* eslint-disable react/prop-types */
+class InputField extends PureComponent {
+  static defaultProps = {
+    lineDirection: 'center',
+    fullWidth: true,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: props.input.value,
+    };
+  }
+
+  handleInputChange = (dummy, event) => {
+    const { input: { onChange } } = this.props;
+
+    this.setState({
+      value: event.target.value
+    });
+
+    if (onChange) {
+      onChange(event);
+    }
+  }
+
+  render() {
+    const { input: { value, onChange, ...input }, meta: { touched, error }, ...others } = this.props;
+
+    return (
+      <TextField
+        id={input.name}
+        error={touched && !!error}
+        errorText={error}
+        onChange={this.handleInputChange}
+        value={this.state.value}
+        {...input}
+        {...others}
+      />
+    );
+  }
+}
 
 export default InputField;
