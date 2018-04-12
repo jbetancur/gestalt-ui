f1e4ce6c0de119370615f4c1a00649cbf9f4e58c
John Betancur <johnnyazee@gmail.com>
2018-04-12 00:56:29 -0400

    added Resource Details/actions to datafeed

--- src/Modules/DataFeeds/containers/DataFeedEdit.jsx
+++ src/Modules/DataFeeds/containers/DataFeedEdit.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { Row, Col } from 'react-flexybox';
 import { withDatafeed, withPickerData } from 'Modules/MetaResource';
+import { withEntitlements } from 'Modules/Entitlements';
 import { Form } from 'react-final-form';
 import { ActivityContainer } from 'components/ProgressIndicators';
 import DataFeedForm from './DataFeedForm';
@@ -14,11 +15,13 @@ import { generatePatches } from '../payloadTransformer';
 
 class DataFeedEdit extends Component {
   static propTypes = {
-    history: PropTypes.object.isRequired,
-    match: PropTypes.object.isRequired,
     datafeedActions: PropTypes.object.isRequired,
     datafeed: PropTypes.object.isRequired,
+    initialFormValues: PropTypes.object.isRequired,
     datafeedPending: PropTypes.bool.isRequired,
+    entitlementActions: PropTypes.func.isRequired,
+    history: PropTypes.object.isRequired,
+    match: PropTypes.object.isRequired,
     secretsData: PropTypes.array.isRequired,
   };
 
@@ -28,17 +31,23 @@ class DataFeedEdit extends Component {
     datafeedActions.fetchDatafeed({ fqon: match.params.fqon, id: match.params.datafeedId });
   }
 
+  onShowEntitlements = () => {
+    const { entitlementActions, datafeed, match } = this.props;
+
+    entitlementActions.showEntitlementsModal(datafeed.name, match.params.fqon, datafeed.id, 'datafeeds', 'Data Feed');
+  }
+
   onSubmit = (values) => {
-    const { match, history, datafeedActions, datafeed } = this.props;
+    const { match, history, datafeedActions, initialFormValues } = this.props;
     const onSuccess = response =>
       history.replace(`/${match.params.fqon}/hierarchy/${match.params.workspaceId}/environment/${match.params.environmentId}/datafeeds/${response.id}`);
-    const payload = generatePatches(datafeed, values);
+    const payload = generatePatches(initialFormValues, values);
 
     datafeedActions.updateDatafeed({ fqon: match.params.fqon, id: match.params.datafeedId, payload, onSuccess });
   };
 
   render() {
-    const { datafeedPending, datafeed, secretsData } = this.props;
+    const { datafeedPending, datafeed, secretsData, initialFormValues } = this.props;
 
     return (
       datafeedPending && !datafeed.id ?
@@ -46,13 +55,16 @@ class DataFeedEdit extends Component {
         <Row justifyContent="center">
           <Col flex={8} xs={12} sm={12} md={10}>
             <Form
+              title={datafeed.name}
+              editMode
               onSubmit={this.onSubmit}
-              initialValues={datafeed}
+              initialValues={initialFormValues}
               render={DataFeedForm}
               validate={validate}
               loading={datafeedPending}
-              editMode
               secrets={secretsData}
+              onShowEntitlements={this.onShowEntitlements}
+              datafeed={datafeed}
             />
           </Col>
         </Row>
@@ -61,11 +73,12 @@ class DataFeedEdit extends Component {
 }
 
 const mapStatetoProps = state => ({
-  datafeed: getDatafeed(state),
+  initialFormValues: getDatafeed(state),
 });
 
 export default compose(
   withPickerData({ entity: 'secrets', label: 'Secrets' }),
+  withEntitlements,
   withDatafeed,
   withRouter,
   connect(mapStatetoProps)
--- src/Modules/DataFeeds/containers/DataFeedForm.jsx
+++ src/Modules/DataFeeds/containers/DataFeedForm.jsx
@@ -9,6 +9,8 @@ import Form from 'components/Form';
 import { Button } from 'components/Buttons';
 import { FullPageFooter } from 'components/FullPage';
 import { Panel } from 'components/Panels';
+import ActionsToolbar from 'components/ActionsToolbar';
+import DetailsPane from 'components/DetailsPane';
 import feedTypes from '../lists/feedTypes';
 
 const getFormats = (values) => {
@@ -36,157 +38,181 @@ const showEditor = (values) => {
   return false;
 };
 
-const DataFeedForm = ({ handleSubmit, submitting, values, match, loading, editMode, secrets }) => (
-  <Form onSubmit={handleSubmit} disabled={loading}>
-    {loading && <ActivityContainer id="datafeed-form" />}
-    <Row gutter={5}>
-      <Col flex={12}>
-        <Panel title="General" expandable={false}>
-          <Row gutter={5}>
-            <Col flex={6} xs={12}>
-              <Field
-                name="name"
-                label="Name"
-                component={TextField}
-                required
-              />
-            </Col>
-          </Row>
+const DataFeedForm = ({ title, datafeed, handleSubmit, submitting, values, match, loading, editMode, secrets, onShowEntitlements }) => (
+  <div>
+    <ActionsToolbar
+      title={title}
+      showActions={editMode}
+      actions={[
+        <Button
+          key="datafeed--entitlements"
+          flat
+          iconChildren="security"
+          onClick={onShowEntitlements}
+        >
+          Entitlements
+        </Button>
+      ]}
+    />
+    <Form onSubmit={handleSubmit} disabled={loading}>
+      {loading && <ActivityContainer id="datafeed-form" />}
+      <Row gutter={5}>
+        {editMode &&
+          <Col flex={12}>
+            <Panel title="Resource Details" defaultExpanded={false}>
+              <DetailsPane model={datafeed} />
+            </Panel>
+          </Col>}
+        <Col flex={12}>
+          <Panel title="General" expandable={false}>
+            <Row gutter={5}>
+              <Col flex={6} xs={12}>
+                <Field
+                  name="name"
+                  label="Name"
+                  component={TextField}
+                  required
+                />
+              </Col>
+            </Row>
 
-          <Row gutter={5}>
-            <Col flex={12}>
-              <Field
-                name="description"
-                label="Description"
-                component={TextField}
-                rows={1}
-              />
-            </Col>
-          </Row>
-        </Panel>
-      </Col>
-
-      <Col flex={12}>
-        <Panel title="Data" expandable={false}>
-          <Row gutter={5}>
-            <Col flex={3} xs={12}>
-              <Field
-                name="properties.kind"
-                label="Type"
-                component={SelectField}
-                itemLabel="displayName"
-                itemValue="type"
-                menuItems={feedTypes}
-                required
-              />
-            </Col>
-
-            <Col flex={3} xs={12}>
-              <Field
-                name="properties.data.format"
-                label="Format"
-                component={SelectField}
-                menuItems={getFormats(values)}
-              />
-            </Col>
-
-            <Col flex={6} xs={12}>
-              <Field
-                name="properties.data.endpoint"
-                label="URI"
-                component={TextField}
-              />
-            </Col>
-
-            <Col flex={6} xs={12}>
-              <Field
-                name="properties.data.topic"
-                label="Topic"
-                component={TextField}
-                required
-              />
-            </Col>
-            <Col flex={6} xs={12}>
-              <Field
-                name="properties.data.group"
-                label="Group"
-                component={TextField}
-              />
-            </Col>
-          </Row>
-
-          <Row gutter={5}>
-            {showEditor(values) &&
+            <Row gutter={5}>
               <Col flex={12}>
                 <Field
-                  component={AceEditor}
-                  mode={getMode(values)}
-                  name="properties.data.data"
-                  maxLines={75}
-                  minLines={5}
+                  name="description"
+                  label="Description"
+                  component={TextField}
+                  rows={1}
                 />
-              </Col>}
-          </Row>
-        </Panel>
-      </Col>
+              </Col>
+            </Row>
+          </Panel>
+        </Col>
 
-      <Col flex={12}>
-        <Panel title="Classification" expandable={false}>
-          <Row gutter={5}>
-            {['Client Data', 'PII', 'HIPPA'].map(item => (
-              <Col flex={2} xs={12} sm={6} md={4} key={item}>
+        <Col flex={12}>
+          <Panel title="Data" expandable={false}>
+            <Row gutter={5}>
+              <Col flex={3} xs={12}>
                 <Field
-                  key={item}
-                  id={item}
-                  label={item}
-                  component={Checkbox}
-                  name="properties.data.classification"
-                  style={{ margin: 0 }}
+                  id="datafeed-type"
+                  name="properties.kind"
+                  label="Type"
+                  component={SelectField}
+                  itemLabel="displayName"
+                  itemValue="type"
+                  menuItems={feedTypes}
+                  required
                 />
-              </Col>))}
-          </Row>
-        </Panel>
-      </Col>
+              </Col>
 
-      <Col flex={12}>
-        <Panel title="Secret" defaultExpanded={!!values.properties.data.secret}>
-          <Row gutter={5}>
-            <Col flex={6} xs={12}>
-              <Field
-                name="properties.data.secret"
-                label="Secret"
-                itemLabel="name"
-                itemValue="id"
-                component={SelectField}
-                menuItems={secrets}
-                async
-              />
-            </Col>
-          </Row>
-        </Panel>
-      </Col>
+              <Col flex={3} xs={12}>
+                <Field
+                  id="datafeed-format"
+                  name="properties.data.format"
+                  label="Format"
+                  component={SelectField}
+                  menuItems={getFormats(values)}
+                />
+              </Col>
 
-      <FullPageFooter>
-        <Button
-          to={`/${match.params.fqon}/hierarchy/${match.params.workspaceId}/environment/${match.params.environmentId}/datafeeds`}
-          flat
-          iconChildren="arrow_back"
-          component={Link}
-        >
-          Data Feeds
-        </Button>
+              <Col flex={6} xs={12}>
+                <Field
+                  name="properties.data.endpoint"
+                  label="URI"
+                  component={TextField}
+                />
+              </Col>
 
-        <Button
-          type="submit"
-          primary
-          raised
-          disabled={submitting}
-        >
-          {editMode ? 'Update' : 'Create'}
-        </Button>
-      </FullPageFooter>
-    </Row>
-  </Form>
+              <Col flex={6} xs={12}>
+                <Field
+                  name="properties.data.topic"
+                  label="Topic"
+                  component={TextField}
+                  required
+                />
+              </Col>
+              <Col flex={6} xs={12}>
+                <Field
+                  name="properties.data.group"
+                  label="Group"
+                  component={TextField}
+                />
+              </Col>
+            </Row>
+
+            <Row gutter={5}>
+              {showEditor(values) &&
+                <Col flex={12}>
+                  <Field
+                    component={AceEditor}
+                    mode={getMode(values)}
+                    name="properties.data.data"
+                    maxLines={75}
+                    minLines={5}
+                  />
+                </Col>}
+            </Row>
+          </Panel>
+        </Col>
+
+        <Col flex={12}>
+          <Panel title="Classification" expandable={false}>
+            <Row gutter={5}>
+              {['Client Data', 'PII', 'HIPPA'].map(item => (
+                <Col flex={2} xs={12} sm={6} md={4} key={item}>
+                  <Field
+                    key={item}
+                    id={item}
+                    label={item}
+                    component={Checkbox}
+                    name="properties.data.classification"
+                    style={{ margin: 0 }}
+                  />
+                </Col>))}
+            </Row>
+          </Panel>
+        </Col>
+
+        <Col flex={12}>
+          <Panel title="Secret" defaultExpanded={!!values.properties.data.secret}>
+            <Row gutter={5}>
+              <Col flex={6} xs={12}>
+                <Field
+                  name="properties.data.secret"
+                  label="Secret"
+                  itemLabel="name"
+                  itemValue="id"
+                  component={SelectField}
+                  menuItems={secrets}
+                  async
+                />
+              </Col>
+            </Row>
+          </Panel>
+        </Col>
+
+        <FullPageFooter>
+          <Button
+            to={`/${match.params.fqon}/hierarchy/${match.params.workspaceId}/environment/${match.params.environmentId}/datafeeds`}
+            flat
+            iconChildren="arrow_back"
+            component={Link}
+          >
+            Data Feeds
+          </Button>
+
+          <Button
+            type="submit"
+            primary
+            raised
+            disabled={submitting}
+          >
+            {editMode ? 'Update' : 'Create'}
+          </Button>
+        </FullPageFooter>
+      </Row>
+    </Form>
+  </div>
 );
 
 DataFeedForm.propTypes = {
@@ -197,10 +223,14 @@ DataFeedForm.propTypes = {
   loading: PropTypes.bool.isRequired,
   editMode: PropTypes.bool,
   secrets: PropTypes.array.isRequired,
+  onShowEntitlements: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  datafeed: PropTypes.object,
 };
 
 DataFeedForm.defaultProps = {
   editMode: false,
+  datafeed: {},
 };
 
 export default withRouter(DataFeedForm);
