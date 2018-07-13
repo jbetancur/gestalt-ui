ec87239d3fe1e66a621e258a56943a6710fdcf1e
John Betancur <johnnyazee@gmail.com>
2018-07-13 11:05:20 -0400

    fix critical bugs 2.1.0

--- src/components/Navigation/NavItem.jsx
+++ src/components/Navigation/NavItem.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import { NavLink } from 'components/Links';
 import styled, { withTheme } from 'styled-components';
 
@@ -11,10 +12,6 @@ const NavLinkStyle = styled(NavLink)`
   width: 100%;
   font-size: 12px;
   cursor: pointer;
-
-  &.active-link * {
-    color: ${props => props.theme.colors['$md-blue-500']} !important;
-  }
 `;
 
 const Text = styled.div`
@@ -26,23 +23,50 @@ const Text = styled.div`
   text-transform: uppercase;
 `;
 
-const NavItem = ({ icon, title, isVisible, ...rest }) => (
-  isVisible ?
-    <NavLinkStyle {...rest}>
-      {icon}
-      <Text>{title}</Text>
-    </NavLinkStyle>
-    : null
-);
+class NavItem extends Component {
+  static propTypes = {
+    icon: PropTypes.object.isRequired,
+    title: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.string,
+    ]).isRequired,
+    isVisible: PropTypes.bool,
+    history: PropTypes.object.isRequired,
+    to: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.string,
+    ]).isRequired,
+  };
 
-NavItem.propTypes = {
-  icon: PropTypes.object.isRequired,
-  title: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
-  isVisible: PropTypes.bool,
-};
+  static defaultProps = {
+    isVisible: true,
+  };
 
-NavItem.defaultProps = {
-  isVisible: true,
-};
+  checkIfShouldNav = (e) => {
+    const { history, to } = this.props;
+    let route = to;
 
-export default withTheme(NavItem);
+    if (typeof to === 'object') {
+      route = to.pathname;
+    }
+
+    if (history.location.pathname === route) {
+      e.preventDefault();
+    }
+  }
+
+  render() {
+    const { icon, title, isVisible, ...rest } = this.props;
+
+    return (
+      isVisible ?
+        <NavLinkStyle onClick={this.checkIfShouldNav} {...rest}>
+          {icon}
+          <Text>{title}</Text>
+        </NavLinkStyle>
+        : null
+    );
+  }
+}
+
+export default withTheme(withRouter(NavItem));
