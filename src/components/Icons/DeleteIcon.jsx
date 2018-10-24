b4d43017a0c6415e44bb6e13db15721ffeb52bdc
John Betancur <johnnyazee@gmail.com>
2018-10-23 23:38:35 -0400

    fix icon styling

--- src/components/Icons/DeleteIcon.jsx
+++ src/components/Icons/DeleteIcon.jsx
@@ -6,7 +6,7 @@ import { SVGIcon } from 'react-md';
 
 // TODO: delete_outline ligature is missing from material-design-icons lib so subing the svg
 const DeleteIcon = ({ size, isRed, ...rest }) => (
-  <SVGIcon viewBox="0 0 24 24" size={size} {...rest}>
+  <SVGIcon viewBox="0 0 24 24" size={size} className="md-icon material-icons" {...rest}>
     <path color={isRed ? '#f44336' : 'none'} d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4z" />
     <path fill="none" d="M0 0h24v24H0V0z" />
   </SVGIcon>
