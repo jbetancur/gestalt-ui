25261bda3c3dd01ca450aa36c82b214877af1622
John Betancur <johnnyazee@gmail.com>
2018-10-25 23:21:49 -0400

    fix icon size

--- src/Modules/Actions/ActionsMenu.jsx
+++ src/Modules/Actions/ActionsMenu.jsx
@@ -2,39 +2,61 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import { get } from 'lodash';
 import axios from 'axios';
 import { ListItem, MenuButton } from 'react-md';
+import { buildParams } from 'config/lib/urlmapper';
 import Div from 'components/Div';
 
 class ActionsMenu extends PureComponent {
   static propTypes = {
     actionList: PropTypes.array,
+    pending: PropTypes.bool,
+    // unloadActions: PropTypes.func.isRequired,
     style: PropTypes.object,
     toggleActionsModal: PropTypes.func.isRequired,
     listItem: PropTypes.bool,
+    model: PropTypes.object,
+    isChildResource: PropTypes.bool,
     icon: PropTypes.bool,
     onActionComplete: PropTypes.func,
+    keyField: PropTypes.string,
+    parentKeyField: PropTypes.string,
+    fqon: PropTypes.string.isRequired,
   };
 
   static defaultProps = {
     actionList: [],
+    pending: false,
     style: { textAlign: 'left' },
     listItem: false,
     icon: false,
     onActionComplete: () => { },
+    keyField: 'id',
+    parentKeyField: 'properties.parent.id',
+    isChildResource: false,
+    model: {},
   };
 
+  setParams() {
+    if (this.props.isChildResource) {
+      return {
+        resource: get(this.props.model, this.props.parentKeyField),
+        pid: get(this.props.model, this.props.keyField),
+      };
+    }
+
+    return { resource: get(this.props.model, this.props.keyField) };
+  }
+
   async fetchContent(action) {
-    const { onActionComplete, toggleActionsModal } = this.props;
+    const url = buildParams(`${this.props.fqon}/actions/${action.id}/ui`, this.setParams());
+    const response = await axios.get(url);
 
-    const verb = action.method
-      ? action.method.toLowerCase()
-      : 'get';
-
-    const response = await axios[verb](action.url);
-
-    if (action.render !== 'none') {
-      toggleActionsModal(response.data, false, onActionComplete);
+    if (action.headless) {
+      this.props.onActionComplete();
+    } else {
+      this.props.toggleActionsModal(response.data, action.full_screen, this.props.onActionComplete);
     }
   }
 
@@ -42,33 +64,29 @@ class ActionsMenu extends PureComponent {
     const { actionList, style } = this.props;
 
     return (
-      actionList.map((action, index) => {
-        const actionName = action.display_name || action.name;
-
-        return (
-          <ListItem
-            key={`${actionName}-${index}`}
-            primaryText={actionName}
-            style={style}
-            onClick={() => this.fetchContent(action)}
-          />
-        );
-      })
+      actionList.map(action => (
+        <ListItem
+          key={action.id}
+          primaryText={action.name}
+          style={style}
+          onClick={() => this.fetchContent(action)}
+        />))
     );
   }
 
   render() {
-    const { actionList, listItem, icon } = this.props;
+    const { pending, actionList, listItem, icon } = this.props;
+
     return (
       actionList && actionList.length > 0 ?
         <Div display="inline">
           {!listItem ?
             <MenuButton
               id="provider-actions-menu"
-              iconChildren={icon ? 'more_vert' : 'arrow_drop_down'}
+              disabled={pending}
+              iconChildren="more_vert"
               flat={!icon}
               icon={icon}
-              iconBefore={false}
               position={MenuButton.Positions.BELOW}
               simplifiedMenu={false}
               repositionOnScroll={false}
