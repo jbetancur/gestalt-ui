270d4bfedf25a357b84c3293497fa43a4da3b469
John Betancur <johnnyazee@gmail.com>
2019-01-08 11:23:05 -0500

    proxy workaround for default log provider

--- nginx/config-and-serve.sh
+++ nginx/config-and-serve.sh
@@ -3,7 +3,8 @@
 <nginx_proxy.conf.template \
       sed "s|META_API_URL|$META_API_URL|" \
     | sed "s|SEC_API_URL|$SEC_API_URL|" \
-    > default.conf 
+    | sed "s|LOG_API_URL|$LOG_API_URL|" \
+    > default.conf
 echo "default.conf"
 cat default.conf
 exec nginx '-g' 'daemon off;'
