160372a0d615d82f14e70c0f201000b7052f0578
John Betancur <johnnyazee@gmail.com>
2019-02-20 23:52:26 -0500

    fix home cards

--- src/components/Cards/CardContent.jsx
+++ src/components/Cards/CardContent.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 const CardContent = styled.div`
   position: relative;
-  overflow: scroll;
   ${props => (props.noPadding ? 'padding: 0' : 'padding: 16px')};
   ${props => (props.minHeight && `min-height: ${props.minHeight}`)};
   ${props => (props.maxHeight && `max-height: ${props.maxHeight}`)};
