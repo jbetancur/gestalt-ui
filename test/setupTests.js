3d3839bc7e8771646b122e0f5e4ad488df7879e9
John Betancur <johnnyazee@gmail.com>
2019-02-21 20:35:54 -0500

    speed up tests

---
+++ test/setupTests.js
@@ -0,0 +1,24 @@
+import 'jest-styled-components';
+
+// localstorage mock
+const localStorageMock = (() => {
+  let store = {};
+  return {
+    getItem(key) {
+      return store[key] || null;
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+});
